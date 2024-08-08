e171012dd97b9cefa85c0f320066099d758f8b54
Sanup Divakaran <sanukyr@gmail.com>
2024-08-08 17:29:19 +0530

    explicit types

---
+++ explicit.ts
@@ -0,0 +1,22 @@
+let eName:String="Harish";
+
+let eId:number=1233;
+
+let isEmployeeOnProbation:boolean = true;
+
+let skills:string[] = ['html','css','bootstrap','react']
+
+let yearOfPassout:number[]= [2016,2018,2022];
+
+let previousOrganization: (number|string)[]=[1,'gmkort',2,'hotprize',3,'jootz']
+
+let address:{hNumber:number; name:string; village:string; city:string; state: string; pin:number}={
+hNumber:123,
+name:'geethamadam',
+village:'kodukku',
+city:'kofgiup',
+state:'karnataka',
+pin:566777
+}
+
+export{}
