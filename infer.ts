e32bbe839e95a68a533179bc6480e010258a9049
Sanup Divakaran <sanukyr@gmail.com>
2024-08-08 15:01:03 +0530

    infer types

---
+++ infer.ts
@@ -0,0 +1,10 @@
+let num=5_000;
+
+number='hello';  
+console.log(num);
+
+// this throws error even if  we doesn't specify the  type for number
+// as ts automatically infers the type to the variable number. 
+// If we hover the variable, we can see the type assigned to it.
+
+// There are many variables for which infer takes place automatically.
