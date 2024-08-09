da050557d3ceb6a7e5d5bda9641204ec55222443
Sanup Divakaran <sanukyr@gmail.com>
2024-08-09 22:26:10 +0530

    interface

---
+++ interface.ts
@@ -0,0 +1,28 @@
+// let person:{name:string,age:number,class:number,teacher:string}={
+//     name:'kumar',
+//     age:23,
+//     class:8,
+//     teacher:'jyothi'
+// }
+
+
+// interface
+
+
+interface Student{
+    name:string;
+    age:number;
+    class:number;
+    teacher:string
+}
+
+
+let person:Student={
+    name:'hari',
+    age: 14,
+    class:8,
+    teacher:'kavya'
+}
+
+
+console.log(typeof person.age);
